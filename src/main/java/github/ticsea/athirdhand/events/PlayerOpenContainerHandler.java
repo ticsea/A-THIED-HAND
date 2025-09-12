a401b87b4c30848d8486cd043ab2c87ff05ac39a
ticsea <ticseaatlan@gmail.com>
2025-09-13 04:47:06 +0800

    fix(switch): fix the switch not work - add switch state monitoring

--- src/main/java/github/ticsea/athirdhand/events/PlayerOpenContainerHandler.java
+++ src/main/java/github/ticsea/athirdhand/events/PlayerOpenContainerHandler.java
@@ -1,6 +1,9 @@
 package github.ticsea.athirdhand.events;
 
+import github.ticsea.athirdhand.client.ModKeyBinding;
+import github.ticsea.athirdhand.config.ModConfig;
 import java.util.List;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -8,6 +11,7 @@ import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.ShulkerBoxMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,17 +19,18 @@ import org.slf4j.LoggerFactory;
 public class PlayerOpenContainerHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlayerOpenContainerHandler.class);
 
+    private static final BooleanValue SWITCH = ModConfig.getSwitch();
+
     private static final List<Class<? extends AbstractContainerMenu>> SUPPORT_CONTAINER_LIST = List.of(ChestMenu.class, ShulkerBoxMenu.class);
 
     public static void onPlayerOpenContainer(PlayerContainerEvent.Open event) {
         Player player = event.getEntity();
         AbstractContainerMenu menu = event.getContainer();
 
-        if (SUPPORT_CONTAINER_LIST.contains(menu.getClass())) {
+        if (SUPPORT_CONTAINER_LIST.contains(menu.getClass()) && SWITCH.get()) {
             transferMatchingItems(menu, player);
             LOGGER.debug("WHAT IS THE MENU: {}", menu);
         }
-
     }
 
     private static void transferMatchingItems(AbstractContainerMenu menu, Player player) {
